31ea1f4cb7cab5c92e6672ed269564819e98318e
ali.arslan <ali.arslan@ANK5-YZLMORT-10>
2022-12-07 11:48:03 +0300

    Add project files.

---
+++ YaziTura1/Classes/Para.cs
@@ -0,0 +1,25 @@
+﻿namespace YaziTura1.Classes
+{
+    /// <summary>
+    /// Yazı Tura için kullanılacak Para sınıfı;
+    /// </summary>
+    class Para
+    {
+        bool _yaziMi;
+        Random _random = new Random();
+        /// <summary>
+        /// Bu method random tipinde bir obje üzeriden yazı için true, tura için false sonucu döner.
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool At()
+        {
+            int randomSayi = _random.Next(0, 2);
+            _yaziMi = false;
+            if (randomSayi == 1)  // 1: yazı 0: tura
+                _yaziMi = true;
+            return _yaziMi;
+
+            
+        }
+    }
+}
---
+++ YaziTura1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml;
+using YaziTura1.Classes;
+
+namespace YaziTura1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Para para = new Para();
+            bool yaziMi, tahmin;
+            string tahminGiris;
+            Console.Write("Yazı mı (y), Tura mı (t), Çıkış (ç) ? ");
+            tahminGiris = Console.ReadLine().ToLower().Trim();
+            while (tahminGiris != "ç")
+            {
+
+                yaziMi = para.At();
+                tahmin = tahminGiris == "y" ? true : false;
+                if (tahmin == yaziMi)
+                    Console.WriteLine("Tebrikler!");
+                else
+                    Console.WriteLine("Tekrar Deneyiniz!");
+                Console.Write("Yazı mı (y), Tura mı (t), Çıkış (ç) ? ");
+                tahminGiris = Console.ReadLine().ToLower().Trim();
+            }
+
+
+        }
+    }
+}
